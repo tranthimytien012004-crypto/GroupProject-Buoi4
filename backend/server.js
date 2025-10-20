5cce6aa07fb64efc2169ac1face44fce3faee177
unknown <tranthimytien012004@gmail.com>
2025-10-20 16:46:32 +0700

    hoàn thành hoạt động 7 phần backend

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
-// server.js
+//cập nhật
+//server.js
 const express = require('express');
+const mongoose = require('mongoose');   // Thêm mongoose
 const app = express();
 require('dotenv').config();
 
 // Middleware để xử lý JSON
 app.use(express.json());
 
-//Gán route user 
+// Kết nối MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('✅ MongoDB connected'))
+.catch(err => console.error('❌ MongoDB connection error:', err));
+
+// Gán route user 
 const userRoutes = require('./routes/user');
 app.use('/', userRoutes);
 
@@ -17,3 +27,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
+const mongoose = require("mongoose");
