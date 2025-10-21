6af5c827386e3c4cf0a78acc9596624507b47ee8
unknown <tranthimytien012004@gmail.com>
2025-10-21 15:46:25 +0700

    Add User model

---
+++ backend/models/User.js
@@ -0,0 +1,22 @@
+// models/User.js
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  age: {
+    type: Number,
+    required: false,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
